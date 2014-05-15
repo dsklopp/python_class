8837f1d80916929afd60fcfbeb58b743235e1c3b
git <dsklopp@gmail.com>
2014-05-14 23:21:29 -0700

    Added list generator versus list comprehension example

---
+++ techniques/generator_version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+f=open('afile.txt')
+lines=(" ".join(line.strip().split()[1:]) for line in f )
+count=0
+for line in lines:
+	count+=1
+print(str(count) + " lines present")
+raw_input()
